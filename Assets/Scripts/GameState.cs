a9d1d76e8a9be7388c44a45b3d36a64c0c5866d9
ryusage <blaquer@gmail.com>
2015-01-24 15:26:46 -0500

    Scripts for game state and choice init

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+using System.Collections.Generic;
+using System;
+
+public class GameState : MonoBehaviour {
+
+	public static WeekendEvent ChosenEvent { get; set; }
+	public static List<WeekendEvent> WeekendEvents { get; set; }
+
+	public int PrepFollowers { get; set; }
+	public int NerdFollowers { get; set; }
+	public int JockFollowers { get; set; }
+	public int EmoFollowers { get; set; }
+	public int DramaFollowers { get; set; }
+	public int BandFollowers { get; set; }
+	public int StonerPreFollowers { get; set; }
+	public int ArtistFollowers { get; set; }
+
+	// Use this for initialization
+	void Start () {
+		LoadWeekendEvents();
+	}
+
+	private void LoadWeekendEvents()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/WeekendEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+	public class WeekendEvent
+	{
+		public bool EventHasBeenChosen { get; set; }
+
+		public string ChoiceText { get; set; }
+		public string EventNarrative { get; set; }
+		public string ResultNarrative { get; set; }
+
+		public float PrepFollowersChange { get; set; }
+		public float NerdFollowersChange { get; set; }
+		public float JockFollowersChange { get; set; }
+		public float EmoFollowersChange { get; set; }
+		public float DramaFollowersChange { get; set; }
+		public float BandFollowersChange { get; set; }
+		public float StonerPreFollowersChange { get; set; }
+		public float ArtistFollowersChange { get; set; }
+	}
+}
