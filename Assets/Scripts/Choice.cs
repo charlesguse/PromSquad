da2f31e89546f7968cc19a57dac5ea42226a16d6
Charlie Guse <charlesguse@users.noreply.github.com>
2015-01-24 13:34:41 -0500

    ALmost finished the main game loop.

---
+++ Assets/Scripts/Choice.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace Scene
+{
+    public class Choice : MonoBehaviour
+    {
+        public static int ChoiceMade { get; set; }
+
+        static Choice()
+        {
+            ChoiceMade = -1;
+        }
+
+
+        // ReSharper disable once UnusedMember.Local
+        void Start()
+        {
+
+        }
+
+        // ReSharper disable once UnusedMember.Local
+        void Update()
+        {
+
+        }
+
+        public void Choose()
+        {
+            ChoiceMade = int.Parse(name.Split(' ')[1]);
+        }
+    }
+}
---
+++ Assets/Scripts/ChoiceResolution.cs
@@ -0,0 +1,21 @@
+﻿using Scene;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoiceResolution : MonoBehaviour
+{
+    private Text _resolution;
+
+    // ReSharper disable once UnusedMember.Local
+    private void Start()
+    {
+        _resolution = GetComponent<Text>();
+        _resolution.text = string.Format("Choice made was {0}", Choice.ChoiceMade);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+}
