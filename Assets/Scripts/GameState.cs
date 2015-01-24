0627dcbd96fe28f209d71e8ee9c98b9bb7c6db71
ryusage <blaquer@gmail.com>
2015-01-24 18:27:56 -0500

    Results get updated now

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,122 @@
+﻿using System.Linq;
+using UnityEngine;
+using Assets.Scripts;
+using System.Collections.Generic;
+using System;
+
+// ReSharper disable once CheckNamespace
+public class GameState : MonoBehaviour
+{
+    public List<AudioClip> OneShots = new List<AudioClip>();
+    public List<AudioClip> Ambience = new List<AudioClip>();
+
+    public static WeekendEvent ChosenEvent { get; set; }
+    public static List<WeekendEvent> WeekendEvents { get; set; }
+
+    public static int PrepFollowers { get; set; }
+    public static int NerdFollowers { get; set; }
+    public static int JockFollowers { get; set; }
+    public static int EmoFollowers { get; set; }
+    public static int DramaFollowers { get; set; }
+    public static int BandFollowers { get; set; }
+    public static int StonerFollowers { get; set; }
+    public static int ArtistFollowers { get; set; }
+
+    // ReSharper disable once UnusedMember.Local
+    private void Start()
+    {
+        LoadWeekendEvents();
+    }
+
+    // ReSharper disable once UnusedMember.Local
+    private void OnLevelWasLoaded(int level)
+    {
+        const int DoScene = 3;
+		const int ResultScene = 4;
+
+        if (level == DoScene)
+        {
+            var audioSources = GetComponents<AudioSource>();
+
+            var oneshot = OneShots.FirstOrDefault(x => x.name == ChosenEvent.OneShot);
+            var ambience = Ambience.FirstOrDefault(x => x.name == ChosenEvent.Ambience);
+
+            if (oneshot != null)
+            {
+                audioSources[0].PlayOneShot(oneshot);
+            }
+            if (ambience != null)
+            {
+                audioSources[1].clip = ambience;
+                audioSources[1].Play();
+            }
+        }
+    }
+
+
+
+    private void LoadWeekendEvents()
+    {
+        WeekendEvents = new List<WeekendEvent>
+        {
+			new WeekendEvent
+			{
+				ChoiceText = "Water Park",
+				EventNarrative = "Went to water park!",
+				ResultNarrative = "Water park results...",
+				PrepFollowersChange = 0,
+				NerdFollowersChange = 0,
+				JockFollowersChange = 0,
+				EmoFollowersChange = 0,
+				DramaFollowersChange = 0,
+				BandFollowersChange = 0,
+				StonerFollowersChange = 0,
+				ArtistFollowersChange = 0,
+			},
+			new WeekendEvent
+			{
+				ChoiceText = "House party",
+				EventNarrative = "Went to house party!",
+				ResultNarrative = "House party results...",
+				PrepFollowersChange = 0,
+				NerdFollowersChange = 0,
+				JockFollowersChange = 0,
+				EmoFollowersChange = 0,
+				DramaFollowersChange = 0,
+				BandFollowersChange = 0,
+				StonerFollowersChange = 0,
+				ArtistFollowersChange = 0,
+			},
+			new WeekendEvent
+			{
+				ChoiceText = "Football Game",
+				EventNarrative = "Went to football game!",
+				ResultNarrative = "Football game results...",
+				PrepFollowersChange = 10,
+				NerdFollowersChange = -10,
+				JockFollowersChange = 0,
+				EmoFollowersChange = 0,
+				DramaFollowersChange = 0,
+				BandFollowersChange = 0,
+				StonerFollowersChange = 0,
+				ArtistFollowersChange = 0,
+                OneShot = "football-oneshot",
+                Ambience = "football-ambience",
+			},
+			new WeekendEvent
+			{
+				ChoiceText = "LAN Party",
+				EventNarrative = "Went to LAN party!",
+				ResultNarrative = "LAN party results...",
+				PrepFollowersChange = 0.5f,
+				NerdFollowersChange = 0,
+				JockFollowersChange = 0,
+				EmoFollowersChange = 0,
+				DramaFollowersChange = 0,
+				BandFollowersChange = 0,
+				StonerFollowersChange = 0,
+				ArtistFollowersChange = 0,
+			},
+		};
+    }
+}
---
+++ Assets/Scripts/WeekendEvent.cs
@@ -0,0 +1,32 @@
+﻿namespace Assets.Scripts
+{
+	public class WeekendEvent
+	{
+	    public WeekendEvent()
+	    {
+            ChoiceText = string.Empty;
+            EventNarrative = string.Empty;
+            ResultNarrative = string.Empty;
+            OneShot = string.Empty;
+            Ambience = string.Empty;
+	    }
+
+		public bool EventHasBeenChosen = false;
+
+		public string ChoiceText { get; set; }
+		public string EventNarrative { get; set; }
+		public string ResultNarrative { get; set; }
+
+		public float PrepFollowersChange { get; set; }
+		public float NerdFollowersChange { get; set; }
+		public float JockFollowersChange { get; set; }
+		public float EmoFollowersChange { get; set; }
+		public float DramaFollowersChange { get; set; }
+		public float BandFollowersChange { get; set; }
+		public float StonerFollowersChange { get; set; }
+		public float ArtistFollowersChange { get; set; }
+
+        public string OneShot { get; set; }
+        public string Ambience { get; set; }
+	}
+}
