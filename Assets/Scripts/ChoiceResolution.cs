f47031413880e53c76d30915caa71f981904c896
Charlie Guse <charlesguse@users.noreply.github.com>
2015-01-24 20:09:31 -0500

    Changed the animation controller some. Not very happy with the results.

---
+++ Assets/Scripts/ChoiceResolution.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// ReSharper disable once CheckNamespace
+public class ChoiceResolution : MonoBehaviour
+{
+    private Text _resolution;
+
+    // ReSharper disable once UnusedMember.Local
+    private void Start()
+    {
+        _resolution = GetComponent<Text>();
+        _resolution.text = GameState.ChosenEvent.EventNarrative;
+    }
+}
---
+++ Assets/Scripts/LeaveResultsScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeaveResultsScene : MonoBehaviour
+{
+	public void LoadNextWeekendOrProm()
+	{
+		if (GameState.WeekendsLeft > 0)
+			Application.LoadLevel("Choice");
+		else
+			Application.LoadLevel("End");
+	}
+}
