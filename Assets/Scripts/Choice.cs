82c2db6755f9d72a7724832c62ec0ac774d6f5ed
ryusage <blaquer@gmail.com>
2015-01-24 19:14:39 -0500

    Got event counter working

---
+++ Assets/Scripts/Choice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Assets.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+// ReSharper disable once CheckNamespace
+namespace Scene
+{
+    public class Choice : MonoBehaviour
+    {
+		private Text _choiceText;
+		private WeekendEvent _eventChoice;		
+
+        // ReSharper disable once UnusedMember.Local
+        void Start()
+        {
+			_choiceText = this.GetComponent<Text>();
+			if (_choiceText == null)
+				throw new MissingComponentException("The Choice component needs to have an assoicated Text component on the same class.");
+
+			_eventChoice = GameState.ChooseRandomWeekendEvent();
+			_choiceText.text = _eventChoice.ChoiceText;
+        }
+
+        public void Choose()
+        {
+			GameState.ChosenEvent = _eventChoice;
+			GameState.ChosenEvent.EventHasBeenChosen = true;
+        }
+    }
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Assets.Scripts;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+using System;
+
+// ReSharper disable once CheckNamespace
+public class GameState : MonoBehaviour
+{
+    public List<AudioClip> OneShots = new List<AudioClip>();
+    public List<AudioClip> Ambience = new List<AudioClip>();
+    public List<Sprite> Backgrounds = new List<Sprite>(); 
+
+    public static WeekendEvent ChosenEvent { get; set; }
+    public static List<WeekendEvent> WeekendEvents { get; set; }
+    private static readonly Queue<WeekendEvent> choices = new Queue<WeekendEvent>(4);
+
+	public static int WeekendsLeft = 10;
+
+    public static int PrepFollowers { get; set; }
+    public static int NerdFollowers { get; set; }
+    public static int JockFollowers { get; set; }
+    public static int EmoFollowers { get; set; }
+    public static int DramaFollowers { get; set; }
+    public static int BandFollowers { get; set; }
+    public static int StonerFollowers { get; set; }
+    public static int ArtistFollowers { get; set; }
+
+
+    // ReSharper disable once UnusedMember.Local
+    private void Start()
+    {
+        LoadWeekendEvents();
+    }
+
+    // ReSharper disable once UnusedMember.Local
+    private void OnLevelWasLoaded(int level)
+    {
+        const int ChoiceScene = 2;
+        const int DoScene = 3;
+		const int ResultScene = 4;
+
+        switch (level)
+        {
+            case ChoiceScene:
+                GameState.ChoiceScene();
+                break;
+            case DoScene:
+                this.DoScene();
+                break;
+        }
+    }
+
+    private static void ChoiceScene()
+    {
+        var availableEvents = WeekendEvents.OrderBy(x => Random.Range(int.MinValue, int.MaxValue)).Select(x => x).ToList();
+
+        foreach (var @event in availableEvents)
+        {
+            choices.Enqueue(@event);
+        }
+    }
+
+    private void DoScene()
+    {
+        var audioSources = GetComponents<AudioSource>();
+        var spriteRenderer = GameObject.Find("Background").GetComponent<SpriteRenderer>();
+
+        var oneshot =
+            OneShots.FirstOrDefault(
+                x => String.Equals(x.name, ChosenEvent.OneShot, StringComparison.CurrentCultureIgnoreCase));
+        var ambience =
+            Ambience.FirstOrDefault(
+                x => String.Equals(x.name, ChosenEvent.Ambience, StringComparison.CurrentCultureIgnoreCase));
+        var background =
+            Backgrounds.FirstOrDefault(
+                x => String.Equals(x.name, ChosenEvent.Background, StringComparison.CurrentCultureIgnoreCase));
+
+        if (oneshot != null)
+        {
+            audioSources[0].PlayOneShot(oneshot);
+        }
+        if (ambience != null)
+        {
+            audioSources[1].clip = ambience;
+            audioSources[1].Play();
+        }
+        if (background != null)
+        {
+            spriteRenderer.sprite = background;
+        }
+    }
+
+    public static WeekendEvent ChooseRandomWeekendEvent()
+    {
+        return choices.Dequeue();
+    }
+
+
+
+    private void LoadWeekendEvents()
+    {
+        WeekendEvents = new List<WeekendEvent>
+        {
+			new WeekendEvent
+			{
+				ChoiceText = "Water Park",
+				EventNarrative = "Went to water park!",
+				ResultNarrative = "Water park results...",
+				PrepFollowersChange = 0,
+				NerdFollowersChange = 0,
+				JockFollowersChange = 0,
+				EmoFollowersChange = 0,
+				DramaFollowersChange = 0,
+				BandFollowersChange = 0,
+				StonerFollowersChange = 0,
+				ArtistFollowersChange = 0,
+			},
+			new WeekendEvent
+			{
+				ChoiceText = "House party",
+				EventNarrative = "Went to house party!",
+				ResultNarrative = "House party results...",
+				PrepFollowersChange = 0,
+				NerdFollowersChange = 0,
+				JockFollowersChange = 0,
+				EmoFollowersChange = 0,
+				DramaFollowersChange = 0,
+				BandFollowersChange = 0,
+				StonerFollowersChange = 0,
+				ArtistFollowersChange = 0,
+			},
+			new WeekendEvent
+			{
+				ChoiceText = "Football Game",
+				EventNarrative = "Went to football game!",
+				ResultNarrative = "Football game results...",
+				PrepFollowersChange = 10,
+				NerdFollowersChange = -10,
+				JockFollowersChange = 0,
+				EmoFollowersChange = 0,
+				DramaFollowersChange = 0,
+				BandFollowersChange = 0,
+				StonerFollowersChange = 0,
+				ArtistFollowersChange = 0,
+                OneShot = "football-oneshot",
+                Ambience = "football-ambience",
+                Background = "football"
+			},
+			new WeekendEvent
+			{
+				ChoiceText = "LAN Party",
+				EventNarrative = "Went to LAN party!",
+				ResultNarrative = "LAN party results...",
+				PrepFollowersChange = 0.5f,
+				NerdFollowersChange = 0,
+				JockFollowersChange = 0,
+				EmoFollowersChange = 0,
+				DramaFollowersChange = 0,
+				BandFollowersChange = 0,
+				StonerFollowersChange = 0,
+				ArtistFollowersChange = 0,
+			},
+		};
+    }
+}
