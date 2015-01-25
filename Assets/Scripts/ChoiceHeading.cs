e801a4be402a21261b496e4081eca255d60fda6f
ryusage <blaquer@gmail.com>
2015-01-25 12:52:26 -0500

    Credits screen in

---
+++ Assets/Scripts/ChoiceHeading.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChoiceHeading : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () {
+		var text = GetComponent<Text>();
+		text.text = string.Format("What do we do now? ({0} weeks til Prom)", GameState.WeekendsLeft);
+	}
+}
